cecf250ae2f44055d4a2ff28139e28c2560c1295
audliaaasss <114149501+audliaaasss@users.noreply.github.com>
2025-04-25 09:19:36 +0700

    Audi Aulia Coding Camp 2025

---
+++ IntermediateWeb/Latihan/first-camera-stream-starter-project/scripts/scripts.js
@@ -0,0 +1,25 @@
+async function startup() {
+  function populateTakenPicture(image) {
+    // TODO: show taken picture
+  }
+
+  function getStream() {
+    // TODO: generate camera stream
+  }
+
+  function cameraLaunch(stream) {
+    // TODO: launch camera on video
+  }
+
+  function cameraTakePicture() {
+    // TODO: draw video frame to canvas
+  }
+
+  function init() {
+    // TODO: init
+  }
+
+  init();
+}
+
+window.onload = startup;
