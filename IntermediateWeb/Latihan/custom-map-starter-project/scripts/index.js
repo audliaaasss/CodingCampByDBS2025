a9aafac1b4355883c0b976a67778f5acbc470539
audliaaasss <114149501+audliaaasss@users.noreply.github.com>
2025-04-29 09:00:56 +0700

    Audi Aulia Coding Camp 2025

--- IntermediateWeb/Latihan/custom-map-starter-project/scripts/index.js
+++ IntermediateWeb/Latihan/custom-map-starter-project/scripts/index.js
@@ -12,4 +12,12 @@ const osmTile = L.tileLayer(osmTileUrl, {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
 });
-osmTile.addTo(myMap);
+//osmTile.addTo(myMap);
+// baseTile.addTo(myMap);
+
+// Add MapTiler layer
+const mtLayer = L.maptilerLayer({
+  apiKey: 'MY_API_KEY_ON_MAPTILER',
+  style: 'https://api.maptiler.com/maps/01967f3c-dcdf-727f-b0ea-95a9379451b7/style.json?key=I2dT0tYl6yn84JaZx08a', // Optional
+});
+mtLayer.addTo(myMap);
